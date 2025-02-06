9c3e7ee79427de3189b89c7b10071189f522087c
Rodrigo Costa <rodrigo.costa94@yahoo.com.br>
2025-02-06 16:55:10 -0300

    feito até o modulo 10 (forms no django)

--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from cars.views import cars_view_teste, cars_view, new_car_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars-teste/', cars_view_teste),
+    path('cars/', cars_view, name='cars_list'),
+    path('new_car/', new_car_view, name='new_car')
 ]
 
 
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from cars.models import Car
+from cars.forms import CarModelForm
 
-# Create your views here.
+
+def cars_view(request):    
+    cars = Car.objects.all().order_by('brand__name')     
+    search = request.GET.get('search') 
+    if search:
+        cars = Car.objects.filter(model__icontains=search)        
+
+    return render(request, 'cars.html', {'cars': cars})
+
+
+def new_car_view(request):
+    if request.method == 'POST':
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else:
+        new_car_form = CarModelForm()
+    return render (request, 'new-car.html', {'new_car_form':new_car_form})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def cars_view_teste(request):
+    # return HttpResponse("<h1>Teste</h1>")
+    
+    cars = Car.objects.all().order_by('brand__name')       # mostra todos os carros disponiveis no banco de dados com as marcas em ordem alfabetica 
+     
+    search = request.GET.get('search') # Pega o valor da chave search, que é digitado pelo usuario na barra de urls ( requisição )
+    
+    # cars = Car.objects.filter(brand='2')     # Mostra a marca que tem o id = 2 (chave estrangeira)  ---  ( vai mostrar os carros da marca Chevrolet )
+    
+    ## O uso do "__" indica que estou acessando um campo da minha classe (ou coluna do meu banco de dados). 
+    ## Como foi feito abaixo, estou acessando o campo brand do meu banco de dados e filtrando a palavra Fiat
+    # cars = Car.objects.filter(brand__name='Fiat')    # Mostra todos os carros com a marca Fiat
+    
+    # cars = Car.objects.filter(model='Passat ls 1.5') # Mostra somente o carro especificado (precisa ser exatamente igual, não pode ter erros)
+    
+    ## O uso do "__" indica que estou acessando um campo da minha classe (ou coluna do meu banco de dados). 
+    ## Como foi feito abaixo, estou acessando o campo model do meu banco de dados e filtrando todos os moodelos de carros com a letra p
+    # cars = Car.objects.filter(model__contains='p') # Vai mostrar todos os modelos de carros que contem a letra "p", vai mostrar Passat ls e passat ts
+    
+    if search:
+        # Na barra de busca utilizar depois de /cars/  ((    ?search=marea  )) para filtrar e mostrar apenas o marea
+        cars = Car.objects.filter(model__icontains=search) # O icontains ignora se as letras estão em caixa alta ou em caixa baixa
+        
+
+    return render(request, 'cars-teste.html', {'cars': cars})
+
+
+
+
+
