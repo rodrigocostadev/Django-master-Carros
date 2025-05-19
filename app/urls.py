f3ce115b85268667c27c2f4eb7ce46eb3cd5a2e0
Rodrigo Costa <rodrigo.costa94@yahoo.com.br>
2025-05-19 13:02:36 -0300

    Modulo 11 concluido, iniciar na aula 64 do modulo 12

--- app/urls.py
+++ app/urls.py
@@ -4,13 +4,17 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_view_teste, cars_view, new_car_view
+from accounts.views import register_view, login_view, logout_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name="logout"),
     path('cars-teste/', cars_view_teste),
     path('cars/', cars_view, name='cars_list'),
-    path('new_car/', new_car_view, name='new_car')
+    path('new_car/', new_car_view, name='new_car'),
 ]
 
 
